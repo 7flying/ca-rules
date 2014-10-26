2c2e687886b1b438ed3e656a72805539da1da5e6
7flying <diez.f.irene@gmail.com>
2014-10-26 15:53:59 +0100

    All the cells are created with the grid initialization + updated paint and delete methods

--- carules/carules.py
+++ carules/carules.py
@@ -4,7 +4,9 @@ from grid import Grid
 def main():
 	mygrid = Grid(600, 600, 20)
 	mygrid.paint()
-	mygrid.fill_cell(1, 2)
+	mygrid.fill_cell(1, 3)
+	mygrid.delete_cell(1, 3)
+	mygrid.fill_cell(4, 4)
 
 if __name__ == '__main__':
 	main()
--- carules/grid.py
+++ carules/grid.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Tkinter import *
+from time import sleep
 
 class Grid(object):
 	"""Grid class. Handles the painting."""
@@ -10,27 +11,26 @@ class Grid(object):
 		self.width = width
 		self.heigh = heigh
 		self.cell_side = cell_side
-		self.cell_matrix = []
-		for i in range(self.heigh / self.cell_side):
-			temp = []
-			for j in range(self.width / self.cell_side):
-				temp.append(None)
-			self.cell_matrix.append(temp)
-
-	def paint(self):
-		""" Paints the grid"""
 		self.window = Tk()
 		self.window.title(Grid.title)
 		self.canvas = Canvas(self.window, width=self.width, height=self.heigh)
 		self.canvas.pack()
+		self.cell_matrix = []
+		for i in range(self.heigh / self.cell_side):
+			temp = []
+			for j in range(self.width / self.cell_side):
+				temp.append(self._create_cell(i, j))
+			self.cell_matrix.append(temp)
+
+	def paint(self):
+		""" Paints the grid"""
 		for x in range(self.cell_side, self.width, self.cell_side):
 			self.canvas.create_line(x, 0, x, self.heigh, fill="black")
 		for y in range(self.cell_side, self.heigh, self.cell_side):
 			self.canvas.create_line(0, y, self.width, y, fill="black")
-		self.window.mainloop()
 
-	def fill_cell(self, x, y):
-		""" Paints the cell at (x, y) in the grid."""
+	def _create_cell(self, x, y):
+		""" Creates a cell in the grid"""
 		# X: Top-left. (x1,y1) = ((x * CELL_SIDE), (y * CELL_SIDE))
 		x1 = x * self.cell_side
 		y1 = y * self.cell_side
@@ -38,11 +38,19 @@ class Grid(object):
 		# (y * CELL_SIDE + CELL_SIDE) )
 		x2 = x1 + self.cell_side
 		y2 = y1 + self.cell_side
-		temp_rect = canvas.create_rectangle(x1, y1, x2, y2, fill="blue")
-		#self.cell_matrix[x][y] = temp_rect
-		self.window.update()
+		return self.canvas.create_rectangle(x1, y1, x2, y2, fill="white")
+
+	def fill_cell(self, x, y):
+		""" Paints the cell at (x, y) in the grid."""
+		print "Fill cell %r" % (self.cell_matrix[x][y])
+		sleep(1)
+		self.canvas.itemconfig(self.cell_matrix[x][y], fill="black")
+		self.window.update_idletasks()
 
 	def delete_cell(self, x, y):
 		""" Deletes the cell at (x,y) in the grid."""
-		self.canvas.delete(self.cell_matrix[x][y])
-		self.cell_matrix[x][y] = None
+		print "Delete cell cell %r" % (self.cell_matrix[x][y])
+		sleep(1)
+		self.canvas.itemconfig(self.cell_matrix[x][y], fill="white")
+		
+		
